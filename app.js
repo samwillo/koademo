00c15be5639e73a678a138f0c94ddedfec67b740
Sam Liu <liuzhenliang@ihealthlabs.com.cn>
2017-06-11 20:22:43 +0800

    add log and time

--- app.js
+++ app.js
@@ -5,9 +5,26 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async (ctx, next) => {
+    //const now = new Date().toLocaleString();
+    console.log(`[INFO] ${Date().toLocaleString()} ${ctx.request.method}  ${ctx.request.url}`);
+    await next();
+});
+
+app.use(async (ctx, next) => {
+    const start = new Date().getTime();
+    await next();
+    const ms = new Date().getTime() - start;
+    console.log(`[INFO] ${Date().toLocaleString()} Time used : ${ms} ms`);
+});
+
+app.use(async (ctx, next) => {
     await next();
     ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, Koa world!</h1>' + '<h2>柳思远，我是爸爸</h2>';
+    ctx.response.body = '<h1>Hello, Koa world!</h1>' +
+        '<h2>柳思远，我是爸爸</h2>' +
+        '<h2>她是妈妈！</h2>' +
+        '<h2>洗衣服的是爷爷</h2>' +
+        '<h2>沙发上的是奶奶</h2>';
 });
 
 app.listen(3000);
